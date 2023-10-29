0358d663fb6b12557597e9ea640cd86e24a7f63a
Nikita <nlevin6@uwo.ca>
2023-10-29 17:25:12 -0400

    get hero info and powers by id implemented

--- client/script.js
+++ client/script.js
@@ -1,16 +1,33 @@
-function getSuperheroInfo() {
+document.getElementById('getInfoButton').addEventListener('click', getSuperheroInfoAndPowers);
+
+function getSuperheroInfoAndPowers() {
     const superheroId = document.getElementById('superheroId').value;
 
-    fetch(`/superhero/${superheroId}`)
-        .then(response => response.json())
-        .then(data => {
-            const superheroInfo = document.getElementById('superheroInfo');
+    Promise.all([
+        fetch(`/superhero/${superheroId}`).then(response => response.json()), //fetch hero info
+        fetch(`/superhero/${superheroId}/powers`).then(response => response.json()) //fetch corresponding hero powers
+    ])
+    .then(data => {
+        const [info, powers] = data;
 
-            if (data.message) {
-                superheroInfo.textContent = data.message;
-            } else {
-                superheroInfo.textContent = JSON.stringify(data, null, 2);
-            }
-        })
-        .catch(error => console.error('Error:', error));
+        const heroInfo = document.getElementById('superheroInfo');
+        const heroPowers = document.getElementById('superheroPowers');
+
+        //hero info
+        if (info.message) {
+            heroInfo.textContent = info.message;
+        } else {
+            heroInfo.textContent = JSON.stringify(info, null, 2);
+        }
+
+        //hero powers
+        if (powers.message) {
+            heroPowers.textContent = powers.message;
+        } else {
+            heroPowers.textContent = JSON.stringify(powers, null, 2);
+        }
+    })
+    .catch(error => {
+        console.error('error: ', error);
+    });
 }
--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.get('/superhero/:id', (req, res) => {
         try {
             const heroInfo = JSON.parse(data);
 
-            //find the hero by ID
+            //find the superhero by ID
             const superhero = heroInfo.find(hero => hero.id === superheroID);
 
             if (superhero) {
@@ -40,6 +40,55 @@ app.get('/superhero/:id', (req, res) => {
         }
     });
 });
+
+//USAGE EXAMPLE: http://localhost:3000/superhero/0/powers. Will give powers for hero with id 0
+app.get('/superhero/:id/powers', (req, res) => {
+    const superheroID = req.params.id;
+
+    fs.readFile('server/superhero_info.json', 'utf8', (err, infoData) => {
+        if (err) {
+            console.error('Error reading superhero_info.json file: ', err);
+            res.status(500).json({ error: 'Error reading superhero_info.json file' });
+            return;
+        }
+        fs.readFile('server/superhero_powers.json', 'utf8', (err, powersData) => {
+            if (err) {
+                console.error('Error reading superhero_powers.json file: ', err);
+                res.status(500).json({ error: 'Error reading superhero_powers.json file' });
+                return;
+            }
+
+            try {
+                const heroInfo = JSON.parse(infoData);
+                const heroPowers = JSON.parse(powersData);
+
+                // Find superhero's name based on the ID
+                const superhero = heroInfo.find(hero => hero.id === parseInt(superheroID));
+
+                if (superhero) {
+                    // Find powers based on the superhero name
+                    const superheroPowers = heroPowers.find(hero => hero.hero_names === superhero.name);
+
+                    if (superheroPowers) {
+                        res.json(superheroPowers); // Send superhero powers as a JSON response
+                    } else {
+                        res.status(404).json({ message: 'Superhero powers not found' });
+                    }
+                } else {
+                    res.status(404).json({ message: 'Superhero not found' });
+                }
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+                res.status(500).json({ error: 'Error parsing JSON' });
+            }
+        });
+    });
+});
+
+
+
+
+
 
 //get superhero_info.json
 app.get('/server/superhero_info.json', (req, res) => {
