b282073969e8d3fdba783aecd91c0919da0fc4d3
Nikita <nlevin6@uwo.ca>
2023-10-29 14:54:35 -0400

    code for GET both json files

--- client/script.js
+++ client/script.js
@@ -1,11 +1,45 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>{
-    res.send("Hello World!");
+//get superhero_info.json
+app.get('/server/superhero_info.json', (req, res) => {
+    console.log(`GET request for ${req.url}`);
+
+    fs.readFile('server/superhero_info.json', 'utf8', (err, data) => {
+        //if reading file encounters an error
+        if (err) {
+            console.error("Error reading the file: ", err);
+            //set response header with code 500, content is sent in plain text
+            res.writeHead(500, { 'Content-Type': 'text/plain' }); 
+            res.end('Error reading the file');
+            return;
+        }
+        //if no error rerading the file
+        res.setHeader('Content-Type', 'application/json');
+        //set status code to 200, send the json file data
+        res.status(200).send(data);
+    });
 });
 
+//get superhero_powers.json
+app.get('/server/superhero_powers.json', (req, res) => {
+    console.log(`GET request for ${req.url}`);
+
+    fs.readFile('server/superhero_powers.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading the file: ", err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error reading the file');
+            return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(data);
+    });
+});
+
+//port listen message
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
